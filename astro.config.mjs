4879dd4406ebc65957f0f78a011fe38759ebd389
Victor Maina <contact@victormaina.com>
2023-06-18 23:29:43 +0300

    remove server integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,4 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
-});
+export default defineConfig({});
